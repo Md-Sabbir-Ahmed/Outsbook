c3d2c5e3c45acff3e3d6065c62f41c3f4df781b1
Sabbir <sabbir106706@gmail.com>
2020-05-28 01:30:05 +0600

    v2-204 using cpp

---
+++ Volume - 2 (200 - 258)/204 - Multi Diffy.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a,b,c,d,ans;
+    cin>>a>>b>>c>>d;
+    ans=(a*b)-(c*d);
+    cout<<ans;
+    return 0;
+}
+
+
