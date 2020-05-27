d117984d65a21cdbb7cfe074e166d3d83a2f9ee0
Sabbir <sabbir106706@gmail.com>
2020-05-27 15:24:15 +0600

    v1-105 using cpp

---
+++ Volume - 1 (100 - 129)/105 - Book Price.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int x,m;
+    cin>>x>>m;
+    int s,r;
+    s=x/7;
+    r=m*s;
+    cout<<r;
+    return 0;
+}
+
+
