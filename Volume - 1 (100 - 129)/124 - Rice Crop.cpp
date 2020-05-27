c3b56ea5a689091ffb17c0073fafd8f939e2a122
Sabbir <sabbir106706@gmail.com>
2020-05-27 23:12:02 +0600

    v1-124 using cpp

---
+++ Volume - 1 (100 - 129)/124 - Rice Crop.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int p1,p2,d1,d2,ans;
+    cin>>p1>>d1>>p2;
+    ans=(p1*d1)/p2;
+    cout<<ans<<" Days";
+    return 0;
+}
+
+
