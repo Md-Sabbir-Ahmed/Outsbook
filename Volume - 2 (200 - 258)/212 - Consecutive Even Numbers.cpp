bd2e7cf5a45547424e1521c768c83d8d2a409a21
Sabbir <sabbir106706@gmail.com>
2020-05-28 02:02:41 +0600

    v2-212 using cpp

---
+++ Volume - 2 (200 - 258)/212 - Consecutive Even Numbers.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n,x;
+    cin>>n;
+    x=(n-12)/4;
+    cout<<x<<" "<<x+2<<" "<<x+4<<" "<<x+6;
+    return 0;
+}
+
