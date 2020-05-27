5bad0035b467444ba5dc484e860e04426c75ae54
Sabbir <sabbir106706@gmail.com>
2020-05-27 15:19:56 +0600

    v1-104 using cpp

---
+++ Volume - 1 (100 - 129)/104 - Ages.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int x;
+    cin>>x;
+    int d,f;
+    d=x/5;
+    f=d*4;
+    cout<<f<<" "<<d;
+    return 0;
+}
+
+
