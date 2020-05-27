d7e700ce300b10565ee391f3174a76bf1667a484
Sabbir <sabbir106706@gmail.com>
2020-05-28 01:11:35 +0600

    v2-200 using cpp

---
+++ Volume - 2 (200 - 258)/200 - Can you type it (II).cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int num1,num2,sum;
+    cin>>num1>>num2;
+    sum=num1+num2;
+    cout<<sum;
+    return 0;
+}
+
+
