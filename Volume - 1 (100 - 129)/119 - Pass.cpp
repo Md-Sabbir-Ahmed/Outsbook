257bf7740d56ac89eb9c57c8e8887cb049a58a06
Sabbir <sabbir106706@gmail.com>
2020-05-27 21:34:36 +0600

    v1-119 using cpp

---
+++ Volume - 1 (100 - 129)/119 - Pass.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    float x,y,ans;
+    cin>>x>>y;
+    ans=((x-y)*100)/x;
+    cout<<ans<<"%";
+    return 0;
+}
+
+
