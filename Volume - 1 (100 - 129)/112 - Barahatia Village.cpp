b1285f9b6a6e0820b9fcb2d30f0bf2e54ba688de
Sabbir <sabbir106706@gmail.com>
2020-05-27 17:29:34 +0600

    v1-112 using cpp

---
+++ Volume - 1 (100 - 129)/112 - Barahatia Village.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int x,p,ans;
+    cin>>x>>p;
+    ans=(x*p)/100;
+    cout<<ans;
+    return 0;
+}
+
+
