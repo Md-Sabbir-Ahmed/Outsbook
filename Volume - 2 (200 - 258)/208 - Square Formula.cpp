777fdb33c924428b61f6ea98969880087639e80d
Sabbir <sabbir106706@gmail.com>
2020-05-28 01:43:23 +0600

    v2-208 using cpp

---
+++ Volume - 2 (200 - 258)/208 - Square Formula.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a,b;
+    cin>>a>>b;
+    cout<<pow((a+b),2);
+    return 0;
+}
+
+
