bb506ec5c1c85e6be6d7b9fe35e7ffca5e9b9fdf
Sabbir <sabbir106706@gmail.com>
2020-05-27 17:41:55 +0600

    v1-113 using cpp

---
+++ Volume - 1 (100 - 129)/113 - Profit Percentage.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int x,y,ans;
+    cin>>x>>y;
+    ans=((y-x)*100)/x;
+    cout<<ans;
+    return 0;
+}
+
+
