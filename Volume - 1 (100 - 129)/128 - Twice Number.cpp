fef1272ec19f17921b4bafc636c73b5b4f440970
Sabbir <sabbir106706@gmail.com>
2020-05-28 00:59:06 +0600

    v1-128 using cpp

---
+++ Volume - 1 (100 - 129)/128 - Twice Number.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int x,s,ans;
+    cin>>x>>s;
+    ans=(s-x)/2;
+    cout<<ans;
+    return 0;
+}
+
+
