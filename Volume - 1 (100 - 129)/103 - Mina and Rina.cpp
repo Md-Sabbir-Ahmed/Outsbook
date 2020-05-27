16a11980a060d19970af79ba49c8472580bceabe
Sabbir <sabbir106706@gmail.com>
2020-05-27 15:13:37 +0600

    v1-103 using cpp

---
+++ Volume - 1 (100 - 129)/103 - Mina and Rina.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int x,y;
+    int r,m;
+    cin>>x>>y;
+    r=(x+y)/2;
+    m=x-r;
+    cout<<r<<" "<<m;
+    return 0;
+}
+
+
+
