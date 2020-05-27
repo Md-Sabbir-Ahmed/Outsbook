24fea3b8644bc2b16c0e411ea6541f8187b1724e
Sabbir <sabbir106706@gmail.com>
2020-05-27 14:51:57 +0600

    v1-102 using cpp

---
+++ Volume - 1 (100 - 129)/102 - Divisor.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int d,q,r;
+    cin>>d>>q>>r;
+    cout<<d/q;
+    return 0;
+}
+
+
