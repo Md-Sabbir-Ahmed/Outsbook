7f2f77a7317f69a669a95aa5ff2a1e1b3095b670
Sabbir <sabbir106706@gmail.com>
2020-05-27 15:34:51 +0600

    v1-106 using cpp

---
+++ Volume - 1 (100 - 129)/106 - Hostel Meal.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int x,m,y,ans;
+    cin>>x>>m>>y;
+    ans=(x*m)/ (x+y);
+    cout<<ans;
+    return 0;
+}
+
+
