09c594db125e9e0290a10a901e9f2f70aa2b48cf
Sabbir <sabbir106706@gmail.com>
2020-05-27 17:52:36 +0600

    v1-114 using cpp

---
+++ Volume - 1 (100 - 129)/114 - Loss Percentage.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int x,y,ans;
+    cin>>x>>y;
+    ans=((x-y)*100)/x;
+    cout<<ans;
+
+    return 0;
+}
+
+
