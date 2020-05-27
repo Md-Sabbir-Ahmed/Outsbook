46f5dc962573c73f1361557e681fd1621d4141ed
Sabbir <sabbir106706@gmail.com>
2020-05-28 01:04:37 +0600

    v1-129 using cpp

---
+++ Volume - 1 (100 - 129)/129 - Subtracted Number.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int x,d,ans;
+    cin>>x>>d;
+    ans=x+d;
+    cout<<ans;
+    return 0;
+}
+
+
