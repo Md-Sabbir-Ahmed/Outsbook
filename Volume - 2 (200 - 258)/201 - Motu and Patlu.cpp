79cd31f6b8219c91cd589aa06b9b5dc0e4c76ad8
Sabbir <sabbir106706@gmail.com>
2020-05-28 01:16:37 +0600

    v2-201 using cpp

---
+++ Volume - 2 (200 - 258)/201 - Motu and Patlu.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int Motu ,Patlu ,sum;
+    cin>>Motu>>Patlu;
+    sum=Motu+Patlu;
+    cout<<sum;
+    return 0;
+}
+
+
