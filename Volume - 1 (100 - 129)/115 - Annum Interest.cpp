9d988c4688b5c46debbd97fabecdd65d058f3d93
Sabbir <sabbir106706@gmail.com>
2020-05-27 18:45:29 +0600

    v1-115 using cpp

---
+++ Volume - 1 (100 - 129)/115 - Annum Interest.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int x,y,ans;
+    float p;
+    cin>>p>>x>>y;
+    float k=p/100;
+    ans=k*x*y;
+    cout<<ans;
+    return 0;
+}
+
+
