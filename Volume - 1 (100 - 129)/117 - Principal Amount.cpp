d4756164f7320ab133eef6f3173dfe7813ad37ad
Sabbir <sabbir106706@gmail.com>
2020-05-27 20:43:04 +0600

    v1-117 using cpp

---
+++ Volume - 1 (100 - 129)/117 - Principal Amount.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    float p,i,y;
+    cin>>p>>i>>y;
+    float x = ((i/y)/p)*100;
+    cout<<x;
+    return 0;
+}
+
+
