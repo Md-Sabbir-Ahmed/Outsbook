f6a4f96a6e84f2e5f73eef054ef2d52d7c00b7bd
Sabbir <sabbir106706@gmail.com>
2020-05-27 19:45:23 +0600

    v1-116 using cpp

---
+++ Volume - 1 (100 - 129)/116 - Interest Rate.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    float x,y,i;
+    cin>>x>>y>>i;
+    float k = ((i/y)/x)*100;
+    cout<<k<<"%\n";
+    return 0;
+}
+
+
