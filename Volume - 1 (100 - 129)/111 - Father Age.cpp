d0852f23ad5b3da70d471d8f0b4bdc128a608fb1
Sabbir <sabbir106706@gmail.com>
2020-05-27 16:51:54 +0600

    v1-111 using cpp

---
+++ Volume - 1 (100 - 129)/111 - Father Age.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int x,y,m,a1,a2;
+    cin>>x>>y>>m;
+    a1=y*3-m;
+    a2=x*3-a1;
+    cout<<a2;
+    return 0;
+}
+
+
