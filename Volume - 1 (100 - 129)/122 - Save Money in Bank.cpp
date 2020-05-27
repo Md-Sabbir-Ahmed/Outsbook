e68b15d73137651f2bb9f6bd5176e2809002c030
Sabbir <sabbir106706@gmail.com>
2020-05-27 22:38:45 +0600

    v1-122 using cpp

---
+++ Volume - 1 (100 - 129)/122 - Save Money in Bank.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t,x,y,ans;
+    cin>>t>>x>>y;
+    ans=(t-x-y)*12;
+    cout<<ans<<" Taka";
+    return 0;
+}
+
+
