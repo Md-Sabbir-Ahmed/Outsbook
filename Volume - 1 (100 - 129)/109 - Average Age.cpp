3c9b05114acf63bfdc765e74cf0767c94623079e
Sabbir <sabbir106706@gmail.com>
2020-05-27 16:28:08 +0600

    v1-109 using cpp

---
+++ Volume - 1 (100 - 129)/109 - Average Age.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int y1,y2,y3,y4,y5,avg;
+    cin>>y1>>y2>>y3>>y4>>y5;
+    avg=(y1+y2+y3+y4+y5)/5;
+    cout<<avg;
+    return 0;
+}
+
+
