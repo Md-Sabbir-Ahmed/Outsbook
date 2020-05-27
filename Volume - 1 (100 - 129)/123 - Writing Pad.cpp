f3d9cfc947a945b728eb192bb57045c112f1e346
Sabbir <sabbir106706@gmail.com>
2020-05-27 22:47:42 +0600

    v1-123 using cpp

---
+++ Volume - 1 (100 - 129)/123 - Writing Pad.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int d,x,y,ans;
+    cin>>d>>x>>y;
+    ans=(x/(12*d))*y;
+    cout<<ans<<" Taka";
+    return 0;
+}
+
+
