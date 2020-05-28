d9fe1e7cc488ad4f8fa1415ce5d50581fd7a2f47
Sabbir <sabbir106706@gmail.com>
2020-05-28 14:20:28 +0600

    v2-224 using cpp

--- Volume - 2 (200 - 258)/224 - Bus Travelling.cpp
+++ Volume - 2 (200 - 258)/224 - Bus Travelling.cpp
@@ -1,1 +1,20 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    float sum=0,avg,a;
+    for(int i=1;i<=7;i++)
+    {
+        cin>>a;
+        sum=sum+a;
+    }
+    avg=sum/7;
+    cout<<avg;
+    return 0;
+}
+
+
