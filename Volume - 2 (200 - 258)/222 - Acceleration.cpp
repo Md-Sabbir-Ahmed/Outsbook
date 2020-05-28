c96cf4a61134ff6df14eca62293ad18042ba5f05
Sabbir <sabbir106706@gmail.com>
2020-05-28 14:06:36 +0600

    v2-222 using cpp

---
+++ Volume - 2 (200 - 258)/222 - Acceleration.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int x,y,t,ans;
+    cin>>x>>y>>t;
+    ans=(y-x)/t;
+    cout<<ans;
+    return 0;
+}
+
+
