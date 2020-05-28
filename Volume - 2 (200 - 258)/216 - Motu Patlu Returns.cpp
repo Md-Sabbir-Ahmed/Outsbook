4f464a721ad77f0a4dad6d630fa26a262e7b2c4e
Sabbir <sabbir106706@gmail.com>
2020-05-28 12:36:55 +0600

    v2-216 using cpp

---
+++ Volume - 2 (200 - 258)/216 - Motu Patlu Returns.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a,ans;
+    cin>>a;
+    ans=a/3;
+    cout<<a-ans<<" "<<ans;
+    return 0;
+}
+
+
