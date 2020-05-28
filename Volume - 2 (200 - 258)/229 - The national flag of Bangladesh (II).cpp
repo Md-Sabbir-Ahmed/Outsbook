4cfac5698252dec11d9d861c3d79ba557e96fbc7
Sabbir <sabbir106706@gmail.com>
2020-05-28 21:38:30 +0600

    v2-229 using cpp

---
+++ Volume - 2 (200 - 258)/229 - The national flag of Bangladesh (II).cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int l;
+    double w,r;
+    cin>>l;
+    r=l/5;
+    w=r*3;
+    cout<<w<<" "<<r;
+    return 0;
+}
+
+
