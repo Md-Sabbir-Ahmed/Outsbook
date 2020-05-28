d13f6a283b6be2358590b822eaf0273ff26169e7
Sabbir <sabbir106706@gmail.com>
2020-05-28 12:47:32 +0600

    v2-218 using cpp

---
+++ Volume - 2 (200 - 258)/218 - Multi Process.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n,ans;
+    cin>>n;
+    ans=((((((n*567)/9)+7492)*235)/47)-498);
+    cout<<ans;
+    return 0;
+}
+
+
