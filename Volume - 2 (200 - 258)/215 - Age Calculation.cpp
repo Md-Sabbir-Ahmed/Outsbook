59de4f241e9ed216ab814b3f14ee456f656eab35
Sabbir <sabbir106706@gmail.com>
2020-05-28 12:32:26 +0600

    v2-215 using cpp

---
+++ Volume - 2 (200 - 258)/215 - Age Calculation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n,ans;
+    cin>>n;
+    ans=26+n;
+    cout<<ans<<" Years 6 Months";
+    return 0;
+}
+
+
