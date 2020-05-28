feb51d8ad06ac36c79f2adcd4e0aaca08334bef1
Sabbir <sabbir106706@gmail.com>
2020-05-28 19:30:04 +0600

    v2-228 using cpp

---
+++ Volume - 2 (200 - 258)/228 - Strange Multiplication.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int m,n,ans;
+    cin>>m>>n;
+    ans=m*n-(m+n);
+    cout<<ans;
+    return 0;
+}
+
+
