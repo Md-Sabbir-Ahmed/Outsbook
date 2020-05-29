24796366e04358d81c27a2b7b6e18aef5c2a65b2
Sabbir <sabbir106706@gmail.com>
2020-05-29 17:40:41 +0600

    v2-239 using cpp

---
+++ Volume - 2 (200 - 258)/239 - Taka Paisa.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t,c;
+    char d;
+    cin>>t>>d>>c;
+    cout<<t<<" Taka "<<c<<" Paisa ";
+    return 0;
+
+}
+
+
