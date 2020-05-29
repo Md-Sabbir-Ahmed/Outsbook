c50d50c401e121d432ba295e75b13ce27e326080
Sabbir <sabbir106706@gmail.com>
2020-05-29 12:56:48 +0600

    v2-237 using cpp

---
+++ Volume - 2 (200 - 258)/237 - Salesman.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int p,m,a;
+    cin>>p;
+    m=(p/3);
+    a=p-m;
+    cout<<"Morning: "<<m<<" Kilograms."<<"\n";
+    cout<<"Afternoon: "<<a<<" Kilograms."<<"\n";
+
+    return 0;
+}
+
+
