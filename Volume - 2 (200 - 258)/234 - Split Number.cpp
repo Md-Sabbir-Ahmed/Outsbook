494f094f42cf1bf8421519a714d035a517c8d5ee
Sabbir <sabbir106706@gmail.com>
2020-05-29 11:16:00 +0600

    v2-234 using cpp

---
+++ Volume - 2 (200 - 258)/234 - Split Number.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n,a,b;
+    cin>>n;
+    a=n/100;
+    b=n%100;
+    cout<<a<<"\n";
+    cout<<b<<"\n";
+    return 0;
+}
+
+
