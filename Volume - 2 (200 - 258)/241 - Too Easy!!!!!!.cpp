1355df2630bbade68e3d5818dc7a383ec612e69b
Sabbir <sabbir106706@gmail.com>
2020-05-29 18:03:51 +0600

    v2-241 using cpp

---
+++ Volume - 2 (200 - 258)/241 - Too Easy!!!!!!.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n,p,d,ans;
+    cin>>n>>p>>d;
+    ans=(d+p)/n;
+    cout<<ans;
+    return 0;
+}
+
+
