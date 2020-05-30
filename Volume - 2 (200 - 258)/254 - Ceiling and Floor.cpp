b48a6cc410111732611c2832844bf0204c929747
Sabbir <sabbir106706@gmail.com>
2020-05-30 17:10:33 +0600

    v2-254 using cpp

---
+++ Volume - 2 (200 - 258)/254 - Ceiling and Floor.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    float m;
+    cin>>m;
+    cout<<"Floor "<<floor(m)<<"\n";
+    cout<<"Ceil "<<ceil(m)<<"\n";
+    return 0;
+}
+
+
