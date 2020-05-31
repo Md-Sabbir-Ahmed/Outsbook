f582b6a00e42ae81c5452a2e494ca405debbd935
Sabbir <sabbir106706@gmail.com>
2020-05-31 14:43:07 +0600

    v2-257 using cpp

---
+++ Volume - 2 (200 - 258)/257 - Racing.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    float l,n;
+    cin>>l>>n;
+    float p;
+    p=(n/l)/(2.0*3.1416);
+    cout<<fixed<<setprecision(2)<<p<<" km";
+    return 0;
+}
+
+
