c4b23ebb30f6d8d85c8de35f057826b7f2d5522b
Sabbir <sabbir106706@gmail.com>
2020-06-07 00:58:28 +0600

    v3-311 using cpp

---
+++ Volume - 3 (300 - 375)/311 - Find Big.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+ int  x, y;
+
+ while(cin>>x>>y)
+    {
+
+        if(x>y)
+        {
+            cout<<x<<" "<<"\n";
+        }
+        else{
+            cout<<y<<" "<<"\n";
+        }
+    }
+ return 0;
+}
