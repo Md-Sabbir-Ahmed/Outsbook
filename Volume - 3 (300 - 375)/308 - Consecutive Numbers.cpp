735318b3db5d476f6a46c4c6af9f7fe74f9a70b8
Sabbir <sabbir106706@gmail.com>
2020-06-06 19:51:27 +0600

    v3-308 using cpp

---
+++ Volume - 3 (300 - 375)/308 - Consecutive Numbers.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+ int  a,x,y,z;
+
+ while(cin>>a)
+    {
+     x=(a-3)/3;
+     y=x+1;
+     z=x+2;
+     cout<<x<<" "<<y<<" "<<z<<"\n";
+    }
+ return 0;
+}
