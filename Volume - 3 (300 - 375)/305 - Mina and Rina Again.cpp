69a341ceb9a80b61906d774db6b88da67d7867ab
Sabbir <sabbir106706@gmail.com>
2020-06-06 19:31:35 +0600

    v3-305 using cpp

---
+++ Volume - 3 (300 - 375)/305 - Mina and Rina Again.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+ int  x, y,r,m;
+
+ while(cin>>x>>y)
+    {
+
+        r=(x-y)/2;
+        m=x-r;
+        cout<<m<<" "<<r<<"\n";
+    }
+ return 0;
+}
