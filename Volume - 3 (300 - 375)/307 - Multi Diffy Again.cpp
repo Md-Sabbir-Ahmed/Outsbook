c3dcac29c9a8fa22d26b6e0ab0fe445689f88856
Sabbir <sabbir106706@gmail.com>
2020-06-06 19:43:33 +0600

    v3-307 using cpp

---
+++ Volume - 3 (300 - 375)/307 - Multi Diffy Again.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+ int  a,b,c,d,ans;
+
+ while(cin>>a>>b>>c>>d)
+    {
+     ans=(a*b)-(c*d);
+     cout<<ans<<"\n";
+    }
+ return 0;
+}
+
