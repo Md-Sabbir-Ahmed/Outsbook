ca97bdf6e32e5e2a08d72ee7a38609682cd16b47
Sabbir <sabbir106706@gmail.com>
2020-06-06 19:39:39 +0600

    v3-306 using cpp

---
+++ Volume - 3 (300 - 375)/306 - Book Price Again.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+ int  x, one ,m,ans;
+
+ while(cin>>x>>m)
+    {
+     one=(x/7);
+     ans=m*one;
+     cout<<ans<<"\n";
+    }
+ return 0;
+}
