cf4996c0d13301596ed6900274232a1d959f0c26
Sabbir <sabbir106706@gmail.com>
2020-06-09 19:55:42 +0600

    v3-318 using cpp

---
+++ Volume - 3 (300 - 375)/318 - Chocolate Bars Again.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m,n,t,r;
+    while(cin>>m>>n)
+    {
+        r=n-m;
+        t=3*r;
+        cout<<m+r+t<<"\n";
+    }
+    return 0;
+}
+
+
