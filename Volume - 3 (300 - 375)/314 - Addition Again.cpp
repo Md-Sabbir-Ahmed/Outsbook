5210cc684af755e744e84be39c7248f9d06e2665
Sabbir <sabbir106706@gmail.com>
2020-06-11 12:12:36 +0600

    v3-314 using cpp

---
+++ Volume - 3 (300 - 375)/314 - Addition Again.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    long long a,b,ans;
+    while(cin>>a>>b)
+    {
+        ans=a+b;
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
+
+
