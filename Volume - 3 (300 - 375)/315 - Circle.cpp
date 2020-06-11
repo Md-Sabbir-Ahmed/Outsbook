53f8bf6d056c346a55f8167fc3813434fe3af767
Sabbir <sabbir106706@gmail.com>
2020-06-11 12:21:58 +0600

    v3-315 using cpp

---
+++ Volume - 3 (300 - 375)/315 - Circle.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double r,ans,pi;
+    pi=3.1416;
+    while(cin>>r)
+    {
+       ans=pi*r*r;
+       cout<<fixed<<setprecision(2)<<ans<<"\n";
+    }
+    return 0;
+}
+
+
