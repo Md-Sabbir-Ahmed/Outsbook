54846439bae217c397866085dc10c7910a335563
Sabbir <sabbir106706@gmail.com>
2020-06-12 14:40:43 +0600

    v3-323 using cpp

---
+++ Volume - 3 (300 - 375)/323 - Temperature Conversion.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double f,c;
+    while(cin>>f)
+    {
+        c=(5.0/9)*(f-32);
+        cout<<fixed<<setprecision(2)<<c<<" C"<<"\n";
+    }
+    return 0;
+}
+
+
