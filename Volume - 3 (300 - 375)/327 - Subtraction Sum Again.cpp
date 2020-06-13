4b6bc63f3b1e77a14375972d5b4aee2eea49379e
Sabbir <sabbir106706@gmail.com>
2020-06-14 01:10:38 +0600

    v3-327 using cpp

---
+++ Volume - 3 (300 - 375)/327 - Subtraction Sum Again.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m,n,o,p,ans;
+    while(cin>>m>>n>>o>>p)
+    {
+        ans=(m-n)+(o-p);
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
+
+
