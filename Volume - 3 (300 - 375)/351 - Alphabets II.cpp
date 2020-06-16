98a87dfd8d9487c315a2cb81cae502aeda27992f
Sabbir <sabbir106706@gmail.com>
2020-06-16 19:36:27 +0600

    v3-351 using cpp

---
+++ Volume - 3 (300 - 375)/351 - Alphabets II.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e;
+    while(cin>>a>>b>>c>>d>>e)
+    {
+        if(a==1)
+        {
+            cout<<"A ";
+        }
+        else if(a==2)
+        {
+            cout<<"B ";
+        }
+        else if(a==3)
+        {
+            cout<<"C ";
+        }
+        else if(a==4)
+        {
+            cout<<"D ";
+        }
+        else if(a==5)
+        {
+            cout<<"E ";
+        }if(b==1)
+        {
+            cout<<"A ";
+        }
+        else if(b==2)
+        {
+            cout<<"B ";
+        }
+        else if(b==3)
+        {
+            cout<<"C ";
+        }
+        else if(b==4)
+        {
+            cout<<"D ";
+        }
+        else if(b==5)
+        {
+            cout<<"E ";
+        }if(c==1)
+        {
+            cout<<"A ";
+        }
+        else if(c==2)
+        {
+            cout<<"B ";
+        }
+        else if(c==3)
+        {
+            cout<<"C ";
+        }
+        else if(c==4)
+        {
+            cout<<"D ";
+        }
+        else if(c==5)
+        {
+            cout<<"E ";
+        }if(d==1)
+        {
+            cout<<"A ";
+        }
+        else if(d==2)
+        {
+            cout<<"B ";
+        }
+        else if(d==3)
+        {
+            cout<<"C ";
+        }
+        else if(d==4)
+        {
+            cout<<"D ";
+        }
+        else if(d==5)
+        {
+            cout<<"E ";
+        }
+        if(e==1)
+        {
+            cout<<"A ";
+        }
+        else if(e==2)
+        {
+            cout<<"B ";
+        }
+        else if(e==3)
+        {
+            cout<<"C ";
+        }else if(e==4)
+        {
+            cout<<"D ";
+        }
+        else if(e==5)
+        {
+            cout<<"E ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+
