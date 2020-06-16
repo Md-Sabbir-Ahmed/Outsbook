44e519c510f793dc3ef4c8303c9aa04485ebf992
Sabbir <sabbir106706@gmail.com>
2020-06-16 17:47:42 +0600

    v3-342 using cpp

---
+++ Volume - 3 (300 - 375)/342 - Alphabets.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(n==1){cout<<"A\n";}
+        if(n==2){cout<<"B\n";}
+        if(n==3){cout<<"C\n";}
+        if(n==4){cout<<"D\n";}
+        if(n==5){cout<<"E\n";}
+
+    }
+    return 0;
+}
+
+
