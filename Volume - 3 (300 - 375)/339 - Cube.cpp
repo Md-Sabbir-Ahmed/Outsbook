f8c41c8d604212ebec5b140042682a41aee5b9e4
Sabbir <sabbir106706@gmail.com>
2020-06-16 17:16:36 +0600

    v3-339 using cpp

---
+++ Volume - 3 (300 - 375)/339 - Cube.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,ans;
+    while(cin>>a)
+    {
+        ans=(a*a)*6;
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
+
+
