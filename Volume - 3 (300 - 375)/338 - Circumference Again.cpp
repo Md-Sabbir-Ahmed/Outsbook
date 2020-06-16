cfc2eeac9fdb9c1809165a4aab5a7ffdaa484579
Sabbir <sabbir106706@gmail.com>
2020-06-16 17:09:59 +0600

    v3-338 using cpp

---
+++ Volume - 3 (300 - 375)/338 - Circumference Again.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double c,pi,ans;
+    pi=3.1416;
+    while(cin>>c)
+    {
+        ans=(c/(2*pi));
+        cout<<fixed<<setprecision(2)<<ans<<"\n";
+    }
+    return 0;
+}
