6d3fccd271d2bc49a5760194d7d00652d88c4136
Sabbir <sabbir106706@gmail.com>
2020-06-16 22:09:37 +0600

    v3-359 using cpp

---
+++ Volume - 3 (300 - 375)/359 - Divisible by 2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    while(cin>>n)
+    {
+        if(n%2==0)
+        {
+            cout<<"YES\n";
+        }
+        else
+        {
+            cout<<"NO\n";
+        }
+    }
+    return 0;
+}
+
+
