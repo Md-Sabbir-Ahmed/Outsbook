c3bd6370b82461f8f499a42f45541a9ea376e504
Sabbir <sabbir106706@gmail.com>
2020-06-16 21:55:40 +0600

    v3-354 using cpp

---
+++ Volume - 3 (300 - 375)/354 - Simple Equation.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m,n,x,y;
+    while(cin>>m>>n)
+    {
+        x=(m+n)/2;
+        y=m-x;
+        cout<<x<<" "<<y<<"\n";
+    }
+    return 0;
+}
+
+
