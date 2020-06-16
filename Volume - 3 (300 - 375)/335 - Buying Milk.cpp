4aaff3bc4887513cd38d3cacbcaa8cc2c576d513
Sabbir <sabbir106706@gmail.com>
2020-06-16 16:55:36 +0600

    v3-335 using cpp

---
+++ Volume - 3 (300 - 375)/335 - Buying Milk.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double x,y,ans;
+    while(cin>>x>>y)
+    {
+        ans=y/x;
+        cout<<fixed<<setprecision(3)<<ans<<" liter\n";
+    }
+    return 0;
+}
+
+
