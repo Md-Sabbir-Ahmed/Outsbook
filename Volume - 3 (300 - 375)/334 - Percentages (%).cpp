90592e4a44b790cba6dfc06358ae6c1d8b0b33be
Sabbir <sabbir106706@gmail.com>
2020-06-18 21:32:08 +0600

    v3-334 using cpp

---
+++ Volume - 3 (300 - 375)/334 - Percentages (%).cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char c;
+    double a,b;
+    double ans;
+    while(cin>>a>>b>>c)
+    {
+     cout<<fixed<<setprecision(0)<<floor(a*b/100)<<"\n";
+    }
+    return 0;
+}
+
+
