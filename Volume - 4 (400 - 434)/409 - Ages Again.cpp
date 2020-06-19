73208d1bf951210ebcb706c8a6b9e0c086559153
Sabbir <sabbir106706@gmail.com>
2020-06-20 01:03:07 +0600

    v4-409 using cpp

---
+++ Volume - 4 (400 - 434)/409 - Ages Again.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int x;
+    while(cin>>x){
+    int d,f;
+    d=x/5;
+    f=d*4;
+    cout<<f<<" "<<d<<"\n";
+ }
+    return 0;
+}
+
