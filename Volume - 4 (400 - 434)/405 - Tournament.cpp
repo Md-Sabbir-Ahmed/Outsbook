2bf4efe194463d33c19dcf7fb299c707ec5377b3
Sabbir <sabbir106706@gmail.com>
2020-06-20 00:41:20 +0600

    v4-405 using cpp

---
+++ Volume - 4 (400 - 434)/405 - Tournament.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(n<20)
+        {
+            cout<<"junior\n";
+        }
+        else{
+            cout<<"senior\n";
+        }
+    }
+    return 0;
+}
+
+
