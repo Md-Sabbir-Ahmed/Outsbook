04168bf90d34b242cca10ca92c7b6bcb0c892bbe
Sabbir <sabbir106706@gmail.com>
2020-06-20 01:07:08 +0600

    v4-410 using cpp

---
+++ Volume - 4 (400 - 434)/410 - Odd or Even.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(n%2==0)
+        {
+            cout<<"Even\n";
+        }
+        else{
+            cout<<"Odd\n";
+        }
+    }
+    return 0;
+}
+
+
