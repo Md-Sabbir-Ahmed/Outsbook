f6113754fac6e0222731a9cf2641e7412b0e1eae
Sabbir <sabbir106706@gmail.com>
2020-06-20 01:20:10 +0600

    v4-413 using cpp

---
+++ Volume - 4 (400 - 434)/413 - The national flag of Bangladesh (II) Again.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int l;
+    double w,r;
+    while(cin>>l){
+    r=l/5;
+    w=r*3;
+    cout<<w<<" "<<r<<"\n";
+ }
+    return 0;
+}
