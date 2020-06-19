ca9a03d1dd1aea18954df2da908a238153b2ca4e
Sabbir <sabbir106706@gmail.com>
2020-06-20 02:07:16 +0600

    v4-422 using cpp

---
+++ Volume - 4 (400 - 434)/422 - Pass or Fail.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(n<33)
+        {
+            cout<<"Fail\n";
+        }
+        else{
+            cout<<"Pass\n";
+        }
+    }
+    return 0;
+}
+
+
