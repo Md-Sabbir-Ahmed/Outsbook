76fea1454df7caaf344ffec83f7f9c437a399488
Sabbir <sabbir106706@gmail.com>
2020-06-20 18:15:37 +0600

    v5-504 using cpp

---
+++ Volume - 5 (500 - 572)/504 - Add The Even.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long a,b;
+    while(cin>>a>>b){
+            long long sum=0;
+        for(int i=a;i<=b;i++)
+        {
+            if(i%2==0){
+              sum=sum+i;
+            }
+        }
+        cout<<sum<<"\n";
+    }
+    return 0;
+}
