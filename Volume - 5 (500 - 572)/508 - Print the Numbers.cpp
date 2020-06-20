9f1d72dce3970382f0f4a50fe340a66f3270e9f2
Sabbir <sabbir106706@gmail.com>
2020-06-20 18:26:02 +0600

    v5-508 using cpp

---
+++ Volume - 5 (500 - 572)/508 - Print the Numbers.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            cout<<i<<" "<<i<<" "<<i<<"\n";
+        }
+    }
+    return 0;
+}
+
+
