16c63f60207e784c490a9092d1300beccb4a2239
Sabbir <sabbir106706@gmail.com>
2020-06-20 18:47:14 +0600

    v5-511 using cpp

---
+++ Volume - 5 (500 - 572)/511 - Print “a”.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(n==0)
+        {
+            cout<<"0\n";
+            continue;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            cout<<"a";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+
