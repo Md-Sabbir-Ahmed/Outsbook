09446f716d4e1c5540a4cb76bf5531687733401e
Sabbir <sabbir106706@gmail.com>
2020-06-20 17:42:35 +0600

    v5-500 using cpp

---
+++ Volume - 5 (500 - 572)/500 - Print “Hello”.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        int c=0;
+
+        for(int i=1;i<=n;i++){
+           c++;
+           cout<<"Line "<<c<<": hello\n";
+        }
+    }
+    return 0;
+}
+
+
