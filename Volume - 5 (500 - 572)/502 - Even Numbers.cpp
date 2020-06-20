3fc98c1d29e5d17595f11df7ca4bfbc0c0f0ce8a
Sabbir <sabbir106706@gmail.com>
2020-06-20 17:50:11 +0600

    v5-502 using cpp

---
+++ Volume - 5 (500 - 572)/502 - Even Numbers.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        for(int i=0;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+                cout<<i<<"\n";
+            }
+        }
+    }
+    return 0;
+}
