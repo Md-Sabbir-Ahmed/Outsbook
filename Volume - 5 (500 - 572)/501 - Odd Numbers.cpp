f4ff679b1a00ab841b0f962c84fddc1f11f1ecd1
Sabbir <sabbir106706@gmail.com>
2020-06-20 17:46:38 +0600

    v5-501 using cpp

---
+++ Volume - 5 (500 - 572)/501 - Odd Numbers.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==1)
+            {
+                cout<<i<<"\n";
+            }
+        }
+    }
+    return 0;
+}
+
+
