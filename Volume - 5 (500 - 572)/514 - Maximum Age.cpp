2c95dc8bf6a90c9ac26c7c43eef8a8055349d53e
Sabbir <sabbir106706@gmail.com>
2020-06-21 13:23:13 +0600

    v5-514 using cpp

---
+++ Volume - 5 (500 - 572)/514 - Maximum Age.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n,maximum=-1;
+    long long k;
+    while(cin>>n){
+            if(n==0)
+            {
+                return 0;
+            }
+    for(int i=0;i<n;++i)
+    {
+        cin>>k;
+        if(maximum<k)
+        {
+            maximum=k;
+        }
+    }
+    cout<<maximum;
+  }
+    return 0;
+}
+
