d114e8ad684dcdd4a7ab8a208c6d54197ac7ab72
Sabbir <sabbir106706@gmail.com>
2020-06-21 23:39:58 +0600

    v5-566 using cpp

---
+++ Volume - 5 (500 - 572)/566 - String Array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char s[5][15]={"I","Love","Outsbook","Programming","School"};
+    for(int i=0;i<5;i++)
+    {
+        cout<<s[i]<<"\n";
+    }
+
+    return 0;
+}
+
+
