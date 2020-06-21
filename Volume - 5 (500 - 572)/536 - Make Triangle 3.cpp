71e1421d1c5728e768286c30d8e965ea0f5732a3
Sabbir <sabbir106706@gmail.com>
2020-06-21 18:28:23 +0600

    v5-536 using cpp

---
+++ Volume - 5 (500 - 572)/536 - Make Triangle 3.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        for(int row=n;row>=1;row--)
+        {
+            for(int col=1;n-row>=col;col++)
+            {
+                cout<<" ";
+            }
+            for(int col=1;row>=col;col++)
+            {
+                cout<<"*";
+            }
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+
