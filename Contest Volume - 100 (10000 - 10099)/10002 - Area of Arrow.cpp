ec4da84c5e4ea886596e2566b5bf33cee224e298
Sabbir <sabbir106706@gmail.com>
2020-06-22 13:10:42 +0600

    cv-10002 using cpp

---
+++ Contest Volume - 100 (10000 - 10099)/10002 - Area of Arrow.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        double k=0.5*n*((double)n/2);
+        cout<<fixed<<setprecision(3)<<k<<"\n";
+    }
+    return 0;
+}
+
+
