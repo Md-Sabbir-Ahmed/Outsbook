d4b3b61d3cbd2bc28640f6a71ff014c449f9089a
Sabbir <sabbir106706@gmail.com>
2020-06-22 15:51:28 +0600

    cv-10109 using cpp

---
+++ Contest Volume - 101 (10100 - 10171)/10109 - Khali Kolshi Man.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double n;
+    while(cin>>n)
+    {
+        cout<<fixed<<setprecision(2)<<(n/4)<<"\n";
+    }
+    return 0;
+}
+
+
