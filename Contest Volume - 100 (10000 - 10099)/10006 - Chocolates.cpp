dc693cd95b2ee53499aac40b689f8ce0c43ad672
Sabbir <sabbir106706@gmail.com>
2020-06-22 14:07:14 +0600

    cv-10006 using cpp

---
+++ Contest Volume - 100 (10000 - 10099)/10006 - Chocolates.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(n%2==0)
+        {
+            cout<<"Happy\n";
+        }
+        else{
+            cout<<"Unhappy\n";
+        }
+    }
+    return 0;
+}
+
+
