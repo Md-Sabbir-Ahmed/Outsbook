4d2d7e4bcaeffd9b1aeba39c7c5993e721f4cbcf
Sabbir <sabbir106706@gmail.com>
2020-06-22 13:16:47 +0600

    cv-10004 using cpp

---
+++ Contest Volume - 100 (10000 - 10099)/10004 - Simple Math.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+    while(cin>>x>>y>>z)
+    {
+        cout<<((x*x)+(y*y)-(z*z))<<"\n";
+    }
+    return 0;
+}
+
+
