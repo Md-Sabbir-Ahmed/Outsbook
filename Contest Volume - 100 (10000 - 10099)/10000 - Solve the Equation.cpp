9ac12b38a819db8d52c50da42192def12ffc862a
Sabbir <sabbir106706@gmail.com>
2020-06-22 13:03:29 +0600

    cv-10000 using cpp

---
+++ Contest Volume - 100 (10000 - 10099)/10000 - Solve the Equation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int v,u,a,t;
+    while(cin>>u>>a>>t)
+    {
+        v=(a*t)+u;
+        cout<<v<<"\n";
+    }
+    return 0;
+}
+
+
